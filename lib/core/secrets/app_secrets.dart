7aed1457f38e21ae81a9ecb0d2365445660799e5
Berk Akgül <108866525+akgullberk@users.noreply.github.com>
2026-03-24 15:44:25 +0300

    Implement authentication and blog features with dependency injection

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://gssokmnczwvwvbqxmums.supabase.co';
+  static const supabaseAnonKey =
+      'sb_publishable_Uh25iXfTwtaYuFFlOIf5nQ_qQ-robdB';
+}
